e978457fa3d7aaa6af9f59c924aa277ab75e091c
Marc Schöchlin <schoechlin@osb-alliance.com>
2023-12-09 15:48:17 +0100

    align config optimize comparison (#198)
    closes #91
    
    Signed-off-by: Marc Schöchlin <schoechlin@osb-alliance.com>

--- check_consistency.py
+++ check_consistency.py
@@ -12,10 +12,14 @@ ORGANIZATION = os.environ.get("ORGANIZATION", "SovereignCloudStack")
 def check_repos(gh: Github) -> int:
     existing_repos = set()
     defined_repos = set()
-    errors = 0
+    archived_repos = set()
+    errors_repos = 0
 
-    for repo in gh.get_organization(ORGANIZATION).get_repos(type="public"):
-        existing_repos.add(repo.name)
+    for repo in gh.get_organization(ORGANIZATION).get_repos(type="public", ):
+        if repo.archived:
+            archived_repos.add(repo.name)
+        else:
+            existing_repos.add(repo.name)
 
     repositories_dir = os.path.join("orgs/" + ORGANIZATION + "/repositories")
 
@@ -30,25 +34,35 @@ def check_repos(gh: Github) -> int:
     repos_not_defined = existing_repos.difference(defined_repos)
 
     if len(repos_not_on_github) != 0:
-        print(f"# Repos not on GitHub:\n", file=sys.stderr)
+        out_tmp = [f"\n# Repos not on GitHub:\n"]
         for repo in repos_not_on_github:
-            print(f"* [{repo}](https://www.github.com/{ORGANIZATION}/{repo}/settings)", file=sys.stderr)
-        errors += 1
-        print("\n", file=sys.stderr)
+            if repo not in archived_repos:
+                errors_repos += 1
+                out_tmp.append(f"* [{repo}](https://www.github.com/{ORGANIZATION}/{repo}/settings)")
+        if len(out_tmp) > 1:
+            print("\n".join(out_tmp))
+
+        out_tmp = [f"\n# Archived repos on GitHub, but still defined:\n"]
+        for repo in repos_not_on_github:
+            if repo in archived_repos:
+                out_tmp.append(f"* [{repo}](https://www.github.com/{ORGANIZATION}/{repo}/settings)")
+        if len(out_tmp) > 1:
+            print("\n".join(out_tmp))
 
     if len(repos_not_defined) != 0:
-        print(f"# Repos not defined in github-manager:\n", file=sys.stderr)
+        out_tmp = [f"\n# Repos not defined in github-manager:\n"]
         for repo in repos_not_defined:
-            print(f"* [{repo}](https://www.github.com/{ORGANIZATION}/{repo}/settings)", file=sys.stderr)
-        errors += 1
-        print("\n", file=sys.stderr)
-    return errors
+            out_tmp.append(f"* [{repo}](https://www.github.com/{ORGANIZATION}/{repo}/settings)")
+            errors_repos += 1
+        if len(out_tmp) > 1:
+            print("\n".join(out_tmp))
+    return errors_repos
 
 
 def check_users(gh: Github) -> int:
     data_file = os.path.join("orgs/" + ORGANIZATION + "/data.yaml")
 
-    errors = 0
+    errors_users = 0
     defined_users = dict()
     existing_users = dict()
 
@@ -64,19 +78,20 @@ def check_users(gh: Github) -> int:
     users_not_defined = set(existing_users.keys()).difference(set(defined_users.keys()))
 
     if len(users_not_on_github) != 0:
-        print(f"# Users not assigned to the GitHub organization:\n", file=sys.stderr)
+        out_tmp = [f"\n# Users not (yet) assigned to the github organization:\n"]
         for user in sorted(users_not_on_github):
-            print(f"* [{user} - {defined_users[user]}](https://www.github.com/{user})")
-        errors += 1
-        print("\n", file=sys.stderr)
+            out_tmp.append(f"* [{user} - {defined_users[user]}](https://www.github.com/{user})")
+        if len(out_tmp) > 1:
+            print("\n".join(out_tmp))
 
     if len(users_not_defined) != 0:
-        print(f"# Users which are not member of the github-organization {ORGANIZATION}:\n", file=sys.stderr)
+        out_tmp = [f"\n# Users which are not defined in github manager:\n"]
         for user in sorted(users_not_defined):
-            print(f"* [{user} - {existing_users[user]}](https://www.github.com/{user})")
-        errors += 1
-        print("\n", file=sys.stderr)
-    return errors
+            out_tmp.append(f"* [{user} - {existing_users[user]}](https://www.github.com/{user})")
+            errors_users += 1
+        if len(out_tmp) > 1:
+            print("\n".join(out_tmp))
+    return errors_users
 
 
 gh = github.Github(login_or_token=API_TOKEN)
@@ -85,8 +100,10 @@ errors = 0
 errors += check_repos(gh)
 errors += check_users(gh)
 
+if errors > 0:
+    print(f"\n{errors} errors detected", file=sys.stderr)
+
 if errors >= 255:
-    print(f"\n{errors} detected", file=sys.stderr)
-    sys.exit(1)
+    sys.exit(255)
 else:
     sys.exit(0)
